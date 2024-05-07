98a75c8fd3dc6d25df9889a35fc79a0f10d17a80
Nezha El Fayez <nezhaelfayez@gmail.com>
2024-05-07 14:51:58 +0200

    debugg tilemap : erreur config

--- src/main/java/oumaima_nezha_mehdy/zelda/modele/Environnement.java
+++ src/main/java/oumaima_nezha_mehdy/zelda/modele/Environnement.java
@@ -1,40 +1,36 @@
 package oumaima_nezha_mehdy.zelda.modele;
-
 public class Environnement {
 
     private int map[][];
-
-    public Environnement(int ligne, int colonne) {
-
-        TileMap(ligne,colonne);
+    public Environnement(int ligne, int colonne){
+        creerMap(ligne, colonne);
     }
 
     public int[][] getMap() {
         return map;
     }
 
-    public int[][] TileMap(int ligne, int colonne){
+    public void creerMap(int ligne, int colonne){
         this.map = new int[ligne][colonne];
-        for(int i = 0; i< ligne; i++){
-            for(int y = 0; y< colonne; y++){
-                this.map[i][y] = 1;
+        for(int i = 0; i < ligne; i++){
+            for(int y = 0; y < colonne; y++){
+                this.map[i][y] = 0;
             }
         }
 
-        return map;
     }
 
     public void afficherMap(){
-        for(int i = 0; i<this.map.length; i++){
-            for(int y = 0; y<this.map[i].length; y++){
-                System.out.print(this.map[i][y]);
+        for(int i = 0; i < this.map.length; i++){
+            for(int y = 0; y < this.map[i].length; y++){
+//                System.out.print(this.map[i][y]);
             }
-            System.out.println();
+//            System.out.println();
         }
     }
-
     public void creationCase(){
         this.map[2][3]=1;
+
     }
 
 }
