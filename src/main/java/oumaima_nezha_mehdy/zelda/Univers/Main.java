2329ff950536811651f968d4f5162fb4ab50ad2b
Nezha El Fayez <nezhaelfayez@gmail.com>
2024-05-21 12:17:00 +0200

    resolution de conflit et mise a jours de la branche principal

--- src/main/java/oumaima_nezha_mehdy/zelda/Univers/Main.java
+++ src/main/java/oumaima_nezha_mehdy/zelda/Univers/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Champ champ = new Champ(10,10);
+        Champ champ = new Champ(10,10,new int[5]);
 
         champ.afficherMap();
 
