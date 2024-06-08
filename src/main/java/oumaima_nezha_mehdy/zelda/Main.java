2aeaf8c3fae1fd6a10c36d74f9316649aa40fb53
Nezha El Fayez <nezhaelfayez@gmail.com>
2024-06-08 14:44:21 +0200

    changement de couleur des boutons

--- src/main/java/oumaima_nezha_mehdy/zelda/Main.java
+++ src/main/java/oumaima_nezha_mehdy/zelda/Main.java
@@ -1,29 +1,8 @@
+/*
+Cette classe à pour but de lancer la page d'accueil.
+*/
+
 package oumaima_nezha_mehdy.zelda;
-
-/*import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import oumaima_nezha_mehdy.zelda.controleur.Controleur;
-
-import java.io.IOException;
-
-public class Main extends Application {
-    @Override
-    public void start(Stage stage) throws IOException{
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("vue.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(),1920,1080);
-        Controleur controller = fxmlLoader.getController();
-        stage.setTitle("B-O Hazard");
-        stage.setScene((scene));
-        stage.show();
-
-    }
-
-    public static void main(String[] args) {
-        launch();
-    }
-}*/
 import oumaima_nezha_mehdy.zelda.controleur.AccueilController;
 import javafx.application.Application;
         import javafx.fxml.FXMLLoader;
@@ -33,9 +12,7 @@ import javafx.application.Application;
         import java.io.IOException;
 
 
-/*
-Cette classe à pour but de lancer la page d'accueil.
- */
+
 public class Main extends Application {
     @Override
     public void start(Stage newWindow) throws IOException {
